7c72c1ad613fbf463609bf97d4d4ab2035c5b1bb
Rodrigo Roussado Viegas <79600667+viegasdev@users.noreply.github.com>
2021-10-23 15:45:21 +0100

    Add files via upload

---
+++ calculadora2021.py
@@ -0,0 +1,15 @@
+# Receber o primeiro número
+num1 = input('Insira um número:\n')
+
+# Receber o segundo número
+num2 = input('Insira outro número:\n')
+
+# Transformar os valores recebidos em inteiros
+num1i = int(num1)
+num2i = int(num2)
+
+# Fazer a multiplicação
+num3 = num1i * num2i 
+
+# Apresentar o resultado
+print('O seu resultado é: ',num3)
